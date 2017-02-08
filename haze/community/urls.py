bcdb70886a2c5a26fdf310a8fe683eef862cd638
katebennu <kate.dorrer@gmail.com>
2017-02-08 16:18:29 +0200

    renders game info

--- haze/community/urls.py
+++ haze/community/urls.py
@@ -5,5 +5,5 @@ app_name = 'community'
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),  #default homepage
-    url(r'^games/(?P<id>[0-9]+)/$', views.game_info, name='game_info'),
+    url(r'^games/(?P<game_id>[0-9]+)/$', views.game_info, name='game_info'),
 ]
--- haze/community/views.py
+++ haze/community/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Game
 
 
 def index(request):
@@ -6,5 +7,6 @@ def index(request):
     # return HttpResponse("Hello World!")
 
 
-def game_info(request, id):
-    return render(request, "community/game-info.html", {'id': id})
+def game_info(request, game_id):
+    game = get_object_or_404(Game, id=game_id)
+    return render(request, "community/game-info.html", {'game': game})
