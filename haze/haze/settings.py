6ffeb481c3cd45b4d384b7c7e004d110d7314813
Mikhail Mishin <mishinma1805@gmail.com>
2017-02-11 15:33:31 +0200

    Append to settings the Heroku specifics conditionally

--- haze/haze/settings.py
+++ haze/haze/settings.py
@@ -127,3 +127,20 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Add Heroku environment settings conditionally
+if "DYNO" in os.environ:
+    # Static files collection directory
+    STATIC_ROOT = 'staticfiles'
+
+    # Parse database configuration from $DATABASE_URL
+    import dj_database_url
+
+    DATABASES['default'] = dj_database_url.config()
+
+    # Honor the 'X-Forwarded-Proto' header for request.is_secure()
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+    # Allow all host headers
+    # DEBUG = False
+    ALLOWED_HOSTS = ['*']
