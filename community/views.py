133c467271e3aaf0c65c9555d0f0042f48d53f59
Max Reuter <max.a.reuter@gmail.com>
2017-02-12 19:27:18 +0200

    integrate url and mockup view to play game

--- community/views.py
+++ community/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Game
+from django.contrib.auth.decorators import login_required, permission_required
+from django.contrib.auth.models import User
+from community.models import Game
 
 
 def game_info(request, game_id):
     game = get_object_or_404(Game, id=game_id)
     return render(request, "community/game-info.html", {'game': game})
+
+
+@login_required
+@permission_required('community.play_game', raise_exception=True)
+def play_game(request, game_id):
+    if request.user.owns_game(game_id):
+        game = get_object_or_404(Game, id=game_id)
+        return render(request, "community/game-info.html", {'game': game})
