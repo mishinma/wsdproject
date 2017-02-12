d0096caa7c3ae7d33e721718c5c33ae2ed9752b9
Max Reuter <max.a.reuter@gmail.com>
2017-02-12 09:07:18 +0200

    add login link

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,13 +1,10 @@
 from django.conf.urls import url
-from django.views.generic.edit import CreateView
-from django.contrib.auth.forms import UserCreationForm
+from accounts import views
 
 app_name = 'accounts'
 
 urlpatterns = [
-    url('^register/', CreateView.as_view(
-            template_name='accounts/registration_form.html',
-            form_class=UserCreationForm
-    ), name='register-user'),
+    url('^register/', views.register_player, name='register-user'),
+    url('^login/', views.login_user, name='login-user'),
 
 ]
