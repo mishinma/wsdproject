d51d33c07f04e7adfa5617f74fc0f094338389fa
Mikhail Mishin <mishinma1805@gmail.com>
2017-02-12 13:40:21 +0200

    Use hyphens for url names and templates

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,8 +7,9 @@ from accounts import views
 app_name = 'accounts'
 
 urlpatterns = [
-    url('^register/$', views.register, name='register_user'),
-    url('^login/$', auth_views.login, {'template_name': 'accounts/login_form.html'}, name='login_user'),
-    url('^logout/$', auth_views.logout, name='logout_user')
+    url('^register/$', views.register, name='register-user'),
+    url('^login/$', auth_views.login, {'template_name': 'accounts/login-form.html'},
+        name='login-user'),
+    url('^logout/$', auth_views.logout, name='logout-user')
 ]
 
