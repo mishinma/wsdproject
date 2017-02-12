ad71a546ede5fc59076b95f6ce7cb5fec8e35401
Max Reuter <max.a.reuter@gmail.com>
2017-02-12 19:22:54 +0200

    create proxy class for django user

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,17 @@
-from django.db import models
+from django.contrib.auth.models import User
+from community.models import Game
 
 
 # Create your models here.
 
+class UserMethods(User):
+    def owns_game(self, game_id):
+        try:
+            game = Game.objects.get(id=game_id)
+        except Game.DoesNotExist:
+            return False
+        else:
+            return game.players.filter(id=self.id).exists()
+
+    class Meta:
+        proxy = True
