00f9e00536264ac4c9d62985c4d3204b87cae6e5
Mikhail Mishin <mishinma1805@gmail.com>
2017-02-12 14:23:56 +0200

    Remove index from community

--- community/urls.py
+++ community/urls.py
@@ -4,6 +4,5 @@ from community import views
 app_name = 'community'
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),  #default homepage
     url(r'^games/(?P<game_id>[0-9]+)/$', views.game_info, name='game_info'),
 ]
--- community/views.py
+++ community/views.py
@@ -2,15 +2,6 @@ from django.shortcuts import render, get_object_or_404
 from .models import Game
 
 
-def index(request):
-    return render(request, "community/index.html")
-
-
 def game_info(request, game_id):
     game = get_object_or_404(Game, id=game_id)
     return render(request, "community/game-info.html", {'game': game})
-
-
-def welcome_user(request):
-    user = request.user
-    return render(request, "community/welcome_user.html", {'user': user})
