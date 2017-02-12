23531a15514683f39d8582889363a95dcd0b0c79
Mikhail Mishin <mishinma1805@gmail.com>
2017-02-12 15:20:32 +0200

    Do not pass user to render

--- base/views.py
+++ base/views.py
@@ -2,5 +2,4 @@ from django.shortcuts import render
 
 
 def index(request):
-    user = request.user
-    return render(request, "base/index.html", {'user': user})
+    return render(request, "base/index.html")
