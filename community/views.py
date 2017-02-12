280e3ecf755439b99e8c53004452943502c567e7
Max Reuter <max.a.reuter@gmail.com>
2017-02-12 09:03:57 +0200

    add landing page for user

--- community/views.py
+++ community/views.py
@@ -4,9 +4,13 @@ from .models import Game
 
 def index(request):
     return render(request, "community/index.html")
-    # return HttpResponse("Hello World!")
 
 
 def game_info(request, game_id):
     game = get_object_or_404(Game, id=game_id)
     return render(request, "community/game-info.html", {'game': game})
+
+
+def welcome_user(request):
+    user = request.user
+    return render(request, "community/welcome_user.html", {'user': user})
