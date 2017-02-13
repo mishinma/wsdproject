f1838cd79e805fe6819501f3ebfac71feab28f45
Mikhail Mishin <mishinma1805@gmail.com>
2017-02-13 17:14:11 +0200

    Change owns_game

--- accounts/models.py
+++ accounts/models.py
@@ -5,13 +5,9 @@ from community.models import Game
 # Create your models here.
 
 class UserMethods(User):
+
     def owns_game(self, game_id):
-        try:
-            game = Game.objects.get(id=game_id)
-        except Game.DoesNotExist:
-            return False
-        else:
-            return game.players.filter(id=self.id).exists()
+        return self.games.filter(id=game_id).exists()
 
     class Meta:
         proxy = True
