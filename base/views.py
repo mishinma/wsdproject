b7bc32db0c2330a596fcf4647d626c0f3eafb821
katebennu <kate.dorrer@gmail.com>
2017-02-13 16:00:13 +0200

    index lists existing games

--- base/views.py
+++ base/views.py
@@ -3,5 +3,5 @@ from community.models import Game, Game_Category
 
 
 def index(request):
-    games = Game.objects.all().
-    return render(request, "base/index.html", {'games': games})
+    top_games = Game.objects.all().order_by('players')
+    return render(request, "base/index.html", {'top_games': top_games})
