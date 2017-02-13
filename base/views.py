3a8c68451c79fb8158b6669f60400670ae1cd953
katebennu <kate.dorrer@gmail.com>
2017-02-13 16:26:26 +0200

    sort games by rating

--- base/views.py
+++ base/views.py
@@ -3,5 +3,5 @@ from community.models import Game, Game_Category
 
 
 def index(request):
-    top_games = Game.objects.all().order_by('players')
+    top_games = Game.objects.all().order_by('-rating')
     return render(request, "base/index.html", {'top_games': top_games})
