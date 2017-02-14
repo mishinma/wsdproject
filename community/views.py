bf7ce699436ee20c20a6d31fe08ed559ef98db38
Mikhail Mishin <mishinma1805@gmail.com>
2017-02-14 11:28:24 +0200

    Use functions in play_game view

--- community/views.py
+++ community/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required, permission_required
-from community.models import Game, Game_Score, Game_State
-from django.db.models import Max
+from community.models import Game, Game_Score
 
 
 def game_info(request, game_id):
@@ -14,33 +13,13 @@ def game_info(request, game_id):
 def play_game(request, game_id):
     if request.user.owns_game(game_id):
         game = get_object_or_404(Game, id=game_id)
-        try:
-            scores = Game_Score.objects.get(game=game)
-            user_high_score = scores.filter(player=request.user).aggregate(Max('score'))
-            user_last_score = scores.filter(player=request.user).aggregate(Max('timestamp'))
-            top_players_scores = scores.order_by('score')
-        except Game_Score.DoesNotExist:
-            user_high_score = 0
-            user_last_score = 0
-            top_players_scores = 0
 
+        # TODO: add top scores
         context = {
             'game': game,
-            'user_high_score': user_high_score,
-            'user_last_score': user_last_score,
-            'top_player_scores': top_players_scores,
+            'user_high_score': game.get_user_highest_score(request.user),
+            'user_last_score': game.get_user_latest_score(request.user),
         }
         return render(request, "community/game-play.html", context)
     else:
         return redirect('community:game-info', game_id=game_id)
-
-
-def save_score(request):
-    if request.POST:
-        # HOW TO GET GAME ID?
-        score = Game_Score(player=request.user, game=request.game, score=request.score)
-        score.save()
-
-
-def save_state(request):
-    pass
