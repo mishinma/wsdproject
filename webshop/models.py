cdc5b5f5df62e7dc1a8e09967ec69d5794e4147e
Max Reuter <max.a.reuter@gmail.com>
2017-02-14 17:40:59 +0200

    add model for pending transaction

--- webshop/models.py
+++ webshop/models.py
@@ -2,14 +2,33 @@ from __future__ import unicode_literals
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.utils import timezone
-
+from hashlib import md5
 from community.models import Game
 from django.contrib.auth.models import User
+import time
+
+
+class TransactionManager(models.Manager):
+    def create_from_pending(pending_transaction, ref, amount, result):
+        finished_transaction = Transaction(user=pending_transaction.user,
+                                           pid=pending_transaction.pid,
+                                           ref=ref, amount=amount,
+                                           result=result
+                                           )
+        finished_transaction.save()
+        PendingTransaction.objects.get(pid=pending_transaction.pid).delete()
+        purchase = None
+        if(result == 'success'):
+            purchase = Purchase.objects.create(pid=finished_transaction,
+                                               payer=finished_transaction.user,
+                                               game=pending_transaction.game)
+        return finished_transaction, purchase
 
 
 class Transaction(models.Model):
     # Should the length be fixed?
     pid = models.fields.CharField(max_length=100, primary_key=True)
+    user = models.ForeignKey(User)
     ref = models.fields.CharField(max_length=100)
     amount = models.fields.DecimalField(max_digits=5,
                                         decimal_places=2,
@@ -17,6 +36,19 @@ class Transaction(models.Model):
                                                     MaxValueValidator(999.00)])
     result = models.fields.CharField(max_length=10)
     timestamp = models.DateTimeField(default=timezone.now)
+
+    objects = TransactionManager()
+
+    @staticmethod
+    def generate_new_pid(game, user):
+        return '{}{}{}'.format(game.id, user.username, int(time.time()))
+
+    @staticmethod
+    def generate_checksum(pid, sid, amount, token):
+        checksum_str = "pid={}&sid={}&amount={}&token={}".format(pid, sid,
+                                                                 amount, token)
+        generator = md5(checksum_str.encode('ascii'))
+        return generator.hexdigest()
 
 
 class Gift(models.Model):
@@ -29,3 +61,9 @@ class Purchase(models.Model):
     payer = models.ForeignKey(User)
     game = models.ForeignKey(Game)
     gift = models.ForeignKey(Gift, null=True, blank=True)
+
+
+class PendingTransaction(models.Model):
+    user = models.OneToOneField(User)
+    pid = models.fields.CharField(max_length=100, primary_key=True)
+    game = models.ForeignKey(Game)
