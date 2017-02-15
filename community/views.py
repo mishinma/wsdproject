ae9f4beb41a8f6ce307d21d04e1545ce910bef64
katebennu <kate.dorrer@gmail.com>
2017-02-15 02:37:55 +0200

    fix saving value for state

--- community/views.py
+++ community/views.py
@@ -22,11 +22,7 @@ def play_game(request, game_id):
                 score = Game_Score(score=request.POST.get("score"), game=game, player=request.user)
                 score.save()
 
-            if request.POST.get("messageType") == "SAVE":
-                state_data = request.POST.get("gameState")
-                import pdb; pdb.set_trace()
-                state = Game_State(state_data=json.dumps(state_data), game=game, player=request.user)
-                state.save()
+
 
         # TODO: add top scores
         context = {
@@ -53,6 +49,19 @@ def create_game(request):
         form = GameForm()
     return render(request, 'community/game-form.html', context={'form': form})
 
+
+def save_state(request, game_id):
+    game = get_object_or_404(Game, id=game_id)
+    # {"messageType":"SAVE","gameState":{"playerItems":["A rock"],"score":10}}
+    #data = request.POST.get("gameState")
+    body_unicode = request.body.decode('utf-8')
+    data = json.loads(body_unicode)
+    #data = body['gameState']
+        #json.loads(request.body.decode('utf-8'))
+    #import pdb; pdb.set_trace()
+    state = Game_State(state_data=data, game=game, player=request.user)
+    state.save()
+    return render(request, "community/game-play.html")
 
 def edit_game(request):
     pass
