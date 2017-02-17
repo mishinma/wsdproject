1d1d8bfe938760a7711376b5603afa43762851b1
Mikhail Mishin <mishinma1805@gmail.com>
2017-02-17 20:39:55 +0200

    Use buttons

--- base/views.py
+++ base/views.py
@@ -3,5 +3,6 @@ from community.models import Game
 
 
 def index(request):
-    top_games = Game.objects.all().order_by('-rating')
-    return render(request, "base/index.html", {'top_games': top_games})
+    top_games = Game.objects.order_by('-rating')
+    top_games_w_buttons = Game.objects.attach_buttons(top_games, request.user)
+    return render(request, "base/index.html", {'top_games': top_games_w_buttons})
--- community/views.py
+++ community/views.py
@@ -145,6 +145,7 @@ def edit_game(request, game_id):
 @permission_required('community.add_game', raise_exception=True)
 def my_inventory(request):
     games = Game.objects.games_for_developer(request.user)
+    games = Game.objects.attach_buttons(games, request.user)
     return render(request, 'community/my-inventory.html', context={'games': games})
 
 
@@ -152,6 +153,7 @@ def my_inventory(request):
 @permission_required('community.play_game', raise_exception=True)
 def my_games(request):
     games = Game.objects.games_for_player(request.user)
+    games = Game.objects.attach_buttons(games, request.user)
     return render(request, 'community/my-games.html', context={'games': games})
 
 
