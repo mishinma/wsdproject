d8d89d65680958c50a4ce3f1a412279a94132272
Max Reuter <max.a.reuter@gmail.com>
2017-02-17 15:32:54 +0200

    add inclusion tag to dynamically load categories

--- base/templatetags/extra_template_tags.py
+++ base/templatetags/extra_template_tags.py
@@ -1,5 +1,6 @@
 from django import template
 from django.contrib.auth.models import Group
+from community.models import GameCategory
 
 register = template.Library()
 
@@ -8,3 +9,9 @@ register = template.Library()
 def has_group(user, group_name):
     group = Group.objects.get(name=group_name)
     return group in user.groups.all()
+
+
+@register.inclusion_tag('base/categories.html')
+def show_categories():
+    categories = GameCategory.objects.all()
+    return {'categories': categories}
