59e6b84fb7d4a5e0cabcb12ebc17996e794e54d9
Max Reuter <max.a.reuter@gmail.com>
2017-02-18 17:04:29 +0200

    add endpoints for activation process

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,8 +8,8 @@ app_name = 'accounts'
 
 urlpatterns = [
     url(r'^register/$', views.register, name='register-user'),
-    url(r'^register/success', views.registration_complete, name='registration-complete'),
-    url(r'^login/$', auth_views.login, {'template_name': 'accounts/login-form.html'},
-        name='login-user'),
+    url(r'^register/activate', views.registration_complete, name='registration-complete'),
+    url(r'^register/success', views.registration_success, name='registration-success'),
+    url(r'^login/$', views.login, name='login-user'),
     url('^logout/$', auth_views.logout, name='logout-user')
 ]
