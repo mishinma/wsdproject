427caf1a1fc880ac00361286667ea00fd69f047a
Max Reuter <max.a.reuter@gmail.com>
2017-02-18 19:15:44 +0200

    add check to ensure sales price is lower than actual price

--- community/forms.py
+++ community/forms.py
@@ -10,3 +10,9 @@ class GameForm(forms.ModelForm):
         model = Game
         fields = ['name', 'source_url', 'category', 'description',
                   'price', 'sales_price', 'logo']
+
+    def clean_sales_price(self):
+        sales_price = self.data['sales_price']
+        if sales_price and sales_price >= self.data['price']:
+            raise forms.ValidationError('Sales price cannot be higher than normal price')
+        return self.data['sales_price']
