d5eb21f47731aaa0a7c5c9bd234460bb91a4bf11
Max Reuter <max.a.reuter@gmail.com>
2017-02-19 13:36:02 +0200

    configure social django

--- haze/settings.py
+++ haze/settings.py
@@ -35,6 +35,20 @@ PAYMENT_SECRET_KEY = '***REMOVED***'
 
 SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '***REMOVED***'
 SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = '***REMOVED***'
+SOCIAL_AUTH_USER_MODEL = 'accounts.UserMethods'
+
+SOCIAL_AUTH_PIPELINE = (
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+    'accounts.pipeline.add_to_players'
+)
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
