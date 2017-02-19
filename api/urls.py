e95c017818b7541ec04c747c19af14ebc6ec63ae
Max Reuter <max.a.reuter@gmail.com>
2017-02-19 17:51:42 +0200

    add url names

--- api/urls.py
+++ api/urls.py
@@ -4,10 +4,10 @@ from api import views
 app_name = 'accounts'
 
 urlpatterns = [
-    url(r'^api/v1/games/cat/(?P<category>\w+)/$', views.get_games_of_category),
-    url(r'^api/v1/games/dev/(?P<dev_id>\d+)/$', views.get_games_of_dev),
-    url(r'^api/v1/games/(?P<game_id>\d+)/$', views.get_game_detail),
-    url(r'^api/v1/games/(?P<game_id>\d+)/scores/$', views.get_scores_of_game),
-    url(r'^api/v1/categories/$', views.get_categories),
-    url(r'^api/v1/devs/$', views.get_developers),
+    url(r'^api/v1/games/cat/(?P<category>\w+)/$', views.get_games_of_category, name='cat-games'),
+    url(r'^api/v1/games/dev/(?P<dev_id>\d+)/$', views.get_games_of_dev, name='dev-games'),
+    url(r'^api/v1/games/(?P<game_id>\d+)/$', views.get_game_detail, name='game-detail'),
+    url(r'^api/v1/games/(?P<game_id>\d+)/scores/$', views.get_scores_of_game, name='game-scores'),
+    url(r'^api/v1/categories/$', views.get_categories, name='all-cats'),
+    url(r'^api/v1/devs/$', views.get_developers, name='all-devs'),
 ]
