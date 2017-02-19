20c45ee289529b92251704e54670499e1d44e5d1
Max Reuter <max.a.reuter@gmail.com>
2017-02-19 16:27:13 +0200

    show developer nameme and support jsonp

--- api/views.py
+++ api/views.py
@@ -9,11 +9,14 @@ def get_games_of_category(request, category):
         category = GameCategory.objects.filter(name__iexact=category)
     except GameCategory.DoesNotExist:
         return HttpResponseNotFound
-    games = Game.objects.filter(category=category).values('name', 'developer', 'price')
+    games = Game.objects.filter(category=category).values('name', 'developer__username', 'price')
 
     data = [game for game in games]
-
-    return HttpResponse(json.dumps(data, use_decimal=True), 'application/json')
+    if 'callback' in request.GET:
+        data = '%s(%s)' % (request.GET['callback'], data)
+        return HttpResponse(data, 'text/javascript')
+    else:
+        return HttpResponse(json.dumps(data, use_decimal=True), 'application/json')
 
 
 def get_games_of_dev(request, dev_id):
@@ -26,13 +29,3 @@ def get_game_detail(request, game_id):
 
 def get_scores_of_game(request):
     pass
-
-
-class DecimalEncoder(json.JSONEncoder):
-    def _iterencode(self, o, markers=None):
-        if isinstance(o, decimal.Decimal):
-            # wanted a simple yield str(o) in the next line,
-            # but that would mean a yield on the line with super(...),
-            # which wouldn't work (see my comment below), so...
-            return (str(o) for o in [o])
-        return super(DecimalEncoder, self)._iterencode(o, markers)
